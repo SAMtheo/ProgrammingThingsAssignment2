96947cc5b6eaa1fad7eae181e912e3c014561151
SAMtheo <sam.theobald1@gmail.com>
2019-04-02 18:05:49 +0100

    login and signup buttons

--- app/src/components/loginPage.jsx
+++ app/src/components/loginPage.jsx
@@ -7,6 +7,22 @@ import '../styles/loginPage.css';
 class LoginPage extends Component {
   constructor(props) {
     super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleSignup = this.handleSignup.bind(this);
+  }
+
+  handleLogin() {
+    const email = document.getElementById('login-email').value;
+    const password = document.getElementById('login-password').value;
+
+    console.log(email, password);
+  }
+
+  handleSignup() {
+    const email = document.getElementById('login-email').value;
+    const password = document.getElementById('login-password').value;
+
+    console.log(email, password);
   }
 
   render() {
@@ -14,33 +30,32 @@ class LoginPage extends Component {
       <div className="login-background">
         <Grid container justify="center" alignItems="center">
           <Grid item xs={12} sm={4}>
-            <Card>
+            <Card className="login-card">
               <CardContent className="login-container">
                 <h2>Login Page</h2>
                 <form className="login-form" noValidate autoComplete="off">
                   <TextField
                     id="login-email"
                     label="Email"
-                    // className={classes.textField}
-                    // value={this.state.name}
-                    // onChange={this.handleChange('name')}
+                    type="Email"
                     margin="normal"
                     variant="outlined"
                   />
                   <TextField
                     id="login-password"
                     label="Password"
-                    // className={classes.textField}
-                    // value={this.state.name}
-                    // onChange={this.handleChange('name')}
+                    type="password"
                     margin="normal"
                     variant="outlined"
                   />
                 </form>
               </CardContent>
               <CardActions>
-                <Button variant="contained" color="primary" className="login-button">
+                <Button variant="contained" color="primary" className="login-button" onClick={this.handleLogin}>
                   Login
+                </Button>
+                <Button variant="contained" color="secondary" className="login-button" onClick={this.handleSignup}>
+                  Sign Up
                 </Button>
               </CardActions>
             </Card>
