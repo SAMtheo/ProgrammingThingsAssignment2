d1db48418a106255e9e9cacab956961c6debf1de
Joe Shapiro <b5011296@my.shu.ac.uk>
2019-04-02 18:33:05 +0100

    Initial MQTT commit, framework in for dealing with expected API calls

---
+++ app/src/mqtt.js
@@ -0,0 +1,77 @@
+var mqtt = require('mqtt');
+
+/*
+ * Connect to our broker
+ */
+var client = mqtt.connect('mqtt://test.mosquitto.org');
+
+/*
+ * Topics we're going to be accessing
+ */
+var topicSubscriptions = ["requestAccess", "removeAccess", "checkAccess", "requestTempAccess"];
+
+/*
+ * Connect to our client and subscribe to all the different topics
+ */
+client.on('connect', function () {
+    client.subscribe(topicSubscriptions, function (err) {
+        if (err) {
+            console.log('ERROR: ' + err);
+            return;
+        }
+    });
+});
+
+export function requestAccess(device, room) {
+    var success = false;
+    var jsonReq = {
+        device: device,
+        room: room
+    };
+
+    client.publish('requestAccess', JSON.stringify(jsonReq));
+
+    return success;
+}
+
+export function removeAccess(device, room) {
+    var success = false;
+    var jsonReq = {
+        device: device,
+        room: room
+    };
+
+    client.publish('removeAccess', JSON.stringify(jsonReq));
+
+    return success;
+}
+
+export function checkAccess(device) {
+    var success = false;
+    var jsonReq = {
+        device: device
+    };
+
+    client.publish('checkAccess', JSON.stringify(jsonReq));
+
+    return success;
+}
+
+export function requestTempAccess(device, room) {
+    var success = false;
+    var jsonReq = {
+        device: device,
+        room: room
+    };
+
+    client.publish('requestTempAccess', JSON.stringify(jsonReq));
+
+    return success;
+}
+
+client.on('message', function (topic, message) {
+    // show topic
+    console.log(topic);
+    // message assumed to be in JSON
+    console.log(message.toJSON());
+});
