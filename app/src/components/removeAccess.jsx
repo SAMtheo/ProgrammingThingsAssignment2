29429c48f869eefd53903bcfcc80095b2c682230
SAMtheo <sam.theobald1@gmail.com>
2019-04-03 15:27:33 +0100

    remove access basic functions

--- app/src/components/removeAccess.jsx
+++ app/src/components/removeAccess.jsx
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import {
   Button,
 } from '@material-ui/core';
-import '../styles/requestAccess.css';
+import '../styles/removeAccess.css';
 
-class RequestAccess extends Component {
+class RemoveAccess extends Component {
   constructor(props) {
     super(props);
 
-    this.requestAccessClick = this.requestAccessClick.bind(this);
+    this.removeAccessClick = this.removeAccessClick.bind(this);
   }
 
-  requestAccessClick() {
+  removeAccessClick() {
     const roomID = this.props.roomNumber;
     const userID = this.props.userNumber;
     console.log(roomID, userID);
@@ -19,18 +19,18 @@ class RequestAccess extends Component {
 
   render() {
     return (
-      <div className="requestAccess-container">
-        <h3>Request Access</h3>
+      <div className="removeAccess-container">
+        <h3>Remove Access</h3>
 
         <p>Room ID: {this.props.roomNumber}</p>
         <p>User ID: {this.props.userNumber}</p>
 
-        <Button onClick={this.requestAccessClick} variant="contained" color="primary">
-          Request Access
+        <Button onClick={this.removeAccessClick} variant="contained" color="primary">
+          Remove Access
         </Button>
       </div>
     );
   }
 }
 
-export default RequestAccess;
+export default RemoveAccess;
