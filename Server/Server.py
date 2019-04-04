93a76e410b105fce418c5d1277a7d0508891e2c3
M1K8 <michael@m1k.me>
2019-04-04 16:08:32 +0100

    Implement full server stack

--- Server/Server.py
+++ Server/Server.py
@@ -1,2 +1,86 @@
+import time
+
+doorAcc = { 
+        "0001":[ ("80099E1C", -1) , ("D0BBA61C", -1) ]
+        }
+
+def findId(door, id) :
+        ids = doorAcc.get(door)
+        if ids == None :
+                return []
+        else :
+                f = list(filter(lambda x : id == x[0], doorAcc.get(door)))
+                return list(map(lambda x : x[0], f))
+
+def getPairs(door, id): 
+        ids = doorAcc.get(door)
+        if ids == None:
+                return []
+        else: 
+                return list(filter(lambda x : id == x[0], doorAcc.get(door)))
+
 def query(door,id) :
-    return (door + ":" + "true"    )
+    print (id + " for door " + door) 
+    ids = findId(door,id)
+    print (ids) 
+    if id in ids :
+        return (door + ":true")
+    else :
+        return(door + ":false")
+
+def addDoor(door) :
+    if door in doorAcc.keys() :
+        return
+    else :
+        doorAcc[door] = []
+
+def addIDDoor(door, id) :
+    ids = findId(door, id) 
+    if (id in ids) :
+        return
+    else :
+        doorAcc[door].append((id, -1))
+
+def removeIDDoor(door,id) :
+    ids = getPairs(door,id)
+    for i in ids :
+        doorAcc[door].remove(i)
+    ids = findId(door,id)
+    # id should not be present any more
+    if id in ids :
+        return False
+    else:
+        return True 
+
+def getAllDoorId () :
+        doors = ""
+        for door in doorAcc:
+                doors = doors + door + ","
+        return doors
+
+def checkID(id) : 
+    for door in doorAcc.keys() :
+        found = list(filter(lambda x : id == x[0] , doorAcc.get(door)))
+    return found
+
+def addTime(door,id) :
+    found = list(filter(lambda x : id == x[0]   , doorAcc.get(door)))
+    if found is not None:
+        return False
+    else : 
+        doorAcc[door].append((id, time.time() + 600))
+        return True   
+
+def validTime (id) :
+    if float(id[1]) < 0.0 :
+        return True
+    if float(id[1]) > time.time():
+        return True
+    return False
+
+def t1() :
+        temp ("0001", "80099E1C")
+
+def temp(door,id) :
+    found = list(filter(lambda x : x[0]==id and validTime(x) , doorAcc.get(door)))
+    print (found)
---
+++ Server/WebServer.py
@@ -0,0 +1,65 @@
+import paho.mqtt.client as mqtt # import client
+import Server
+
+broker = "localhost"
+port = 9001
+sub_topic = "#"
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("subbed")
+
+def on_message(client, userdata, message):
+
+    if (str(message.payload.decode('utf-8'))[0] != '#') :
+        print (message.topic)
+        print ("message " + str(message.payload.decode("utf-8")))
+        topic = message.topic
+        m = str(message.payload.decode("utf-8"))
+        m = m.split(":")
+
+        if (topic == "removeAccess") : 
+            res = Server.removeIDDoor(m[0],m[1])
+            if res :
+                response = "True"
+            else: 
+                response = "Are you happy now :rage:"
+            client.publish(topic,"#"+response)
+
+        elif (topic == "requestAccess") :
+            Server.addIDDoor(m[0], m[1])
+            res = Server.query(m[0], m[1])
+            client.publish(topic, "#"+res)
+
+        elif (topic == "requestTempAccess") :
+            res = Server.addTime(m[0],m[1])
+            client.publish(topic, "#"+res)
+
+        elif (topic == "checkAccess") :
+            print(m[0] + "?" + m[1]+ "?")
+            res = Server.query(m[0],m[1])
+            client.publish(topic, "#"+res)
+        elif (topic == "getRooms") : 
+                res = Server.getAllDoorId()
+                client.publish(topic, "#"+res)
+        else : 
+            pass
+    
+def on_publish(client, userdata, mid):
+    print("data published mid=",mid,"\n")
+
+def on_disconnect(client,userdata,rc):
+    print("client disconnected")
+
+client= mqtt.Client("Server",transport='websockets')
+client.on_subscribe = on_subscribe
+client.on_publish = on_publish
+client.on_message= on_message
+client.on_disconnect = on_disconnect
+
+print("connecting")
+client.connect(broker, port)
+client.loop_start()
+client.subscribe(sub_topic)
+
+while 1 :
+    pass
