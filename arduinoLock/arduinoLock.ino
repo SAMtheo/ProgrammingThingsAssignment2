5659f342384170f000905023713b5ae98fc5a88e
b5016752 <b5016752@my.shu.ac.uk>
2019-04-04 18:26:50 +0100

    Added servo into the Arduino lock

--- arduinoLock/arduinoLock.ino
+++ arduinoLock/arduinoLock.ino
@@ -1,14 +1,19 @@
 #include <SL018.h>
 #include <Wire.h>
+#include <Servo.h>
+
+Servo doorServo;
+SL018 rfid;
 
 int RXLED = 17;  // The RX LED has a defined Arduino pin
+int servoPos = 0; // current position of the servo
 String IDTag = "0001:";
-
-SL018 rfid;
 
 void setup() {
   Wire.begin();  
   pinMode(RXLED, OUTPUT);
+  doorServo.attach(10);
+  doorServo.write(servoPos);
 
   Serial1.begin(9600);
   Serial.begin(9600);
@@ -57,8 +62,18 @@ boolean checkLoop() {
 void doorOpen() {
   Serial.println("Door Opening");
   digitalWrite(RXLED, HIGH);   // turn the LED on (HIGH is the voltage level)  
+  // move open
+  for (servoPos; servoPos <= 90; servoPos += 1) {
+    doorServo.write(servoPos);
+    delay(15);    
+  }
 }
 
 void doorClose() {
   digitalWrite(RXLED, LOW);    // turn the LED off by making the voltage LOW  
+  // move shut
+  for (servoPos; servoPos >= 0; servoPos -= 1) {
+    doorServo.write(servoPos);
+    delay(15);
+  }
 }
