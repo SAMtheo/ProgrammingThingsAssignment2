6efbd7342dd0eb24c62b78208e6d374379ccd55f
SAMtheo <sam.theobald1@gmail.com>
2019-04-06 15:03:00 +0100

    comments for header, loading and loginPage

--- app/src/components/header.jsx
+++ app/src/components/header.jsx
@@ -3,18 +3,12 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
 import Button from '@material-ui/core/Button';
 import Firebase from 'firebase';
 
+/**
+ * css styles for the header.
+ */
 const styles = {
     root: {
         flexGrow: 1,
@@ -28,6 +22,9 @@ const styles = {
     },
 };
 
+/**
+ * displays the header, on the top of the homepage.
+ */
 class MenuAppBar extends React.Component {
     state = {
         auth: true,
